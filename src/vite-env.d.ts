32ddc977fdbbdb1c9506cdb793eef6a048731d3a
hexamarkco <159583623+hexamarkco@users.noreply.github.com>
2025-11-12 23:50:13 -0300

    Adjust ImportMetaEnv index signature

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
+interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly [key: string]: string | boolean | undefined;
 }
-
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }

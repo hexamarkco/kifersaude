5973006fe3d91756a6d5fc47d07832801da5bdf7
hexamarkco <159583623+hexamarkco@users.noreply.github.com>
2025-11-19 11:54:39 -0300

    feat: add web push infrastructure

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv extends Readonly<Record<string, string>> {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_SUPABASE_FUNCTIONS_URL: string;
+  readonly VITE_WEB_PUSH_PUBLIC_KEY?: string;
 }
 
 interface ImportMeta {

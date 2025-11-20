ba473d1e2576a4e411250d0f2736d91fc3626ff7
hexamarkco <159583623+hexamarkco@users.noreply.github.com>
2025-11-20 01:14:34 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -39,7 +39,7 @@ createRoot(document.getElementById('root')!).render(
 if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
   const register = () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('/sw.js')
       .then((registration) => {
         console.info('Service worker registrado', registration.scope);
       })
